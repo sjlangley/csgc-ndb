3771843463ae72dc9a027112f487bac113b66426
Stuart Langley <stuart@langleyclan.com>
2024-07-03 11:46:05 +1000

    Move to flask for serving. Still leveraging the existing api backend

--- main.py
+++ main.py
@@ -1,156 +1,132 @@
-"""
 
-"""
-import os
 
-from google.appengine.api import urlfetch
-from google.appengine.api import users
+import hashlib
+import json
+import urllib.request
 
 from datetime import date
-import json
-import jinja2
+from flask import Flask, render_template, request, url_for
+from pymemcache.client.base import Client
 from operator import itemgetter
-from urlparse import urlparse
-import webapp2
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-  loader=jinja2.FileSystemLoader([
-    os.path.dirname(__file__),
-    os.path.dirname(__file__) + '/templates',
-  ]),
-  extensions=['jinja2.ext.autoescape'],
-  autoescape=True)
+def sha1(msg: str) -> str:
+    return hashlib.sha1(msg.encode()).hexdigest()
 
-WOOLOOWARE_WHITE_SLOPE = 118
+class JsonSerde(object):
+    def serialize(self, key, value):
+        if isinstance(value, str):
+            return value.encode('utf-8'), 1
+        return json.dumps(value).encode('utf-8'), 2
 
-class MainPage(webapp2.RequestHandler):
-  """   """
+    def deserialize(self, key, value, flags):
+       if flags == 1:
+           return value.decode('utf-8')
+       if flags == 2:
+           return json.loads(value.decode('utf-8'))
+       raise Exception("Unknown serialization format")
+    
+client = Client('192.168.1.5', serde=JsonSerde())
 
-  def get(self):
-    """   """
-    template_values = _get_standard_template_properties(self.request)
-
-    template = JINJA_ENVIRONMENT.get_template('main.html')
-    self.response.write(template.render(template_values))
-
-
-class AddMembers(webapp2.RequestHandler):
-  """Add new members to the club."""
-
-  def get(self):
-    url = '%s/api/add-members' % self.request.host_url
-
-    template_values = _get_standard_template_properties(self.request)
-    template_values['add_member_post_url'] = url
-    template_values['form_size'] = 10
-
-    template = JINJA_ENVIRONMENT.get_template('add_members.html')
-    self.response.write(template.render(template_values))
+def __retrieve_data(url: str):
+    key = sha1(url)
 
 
-class AddClub(webapp2.RequestHandler):
-  """Add a new club."""
-
-  def get(self):
-    url = '%s/api/add-club' % self.request.host_url
-
-    template_values = _get_standard_template_properties(self.request)
-    template_values['add_club_post_url'] = url
-    template_values['courses_per_club'] = 2
-    template_values['tees_per_course'] = 3
-
-    template = JINJA_ENVIRONMENT.get_template('add_club.html')
-    self.response.write(template.render(template_values))
+    data = client.get(key)
+    if data:
+        app.logger.debug("Key %s found in cache." % url)
+        return data
 
 
-class ListClubGeneral(webapp2.RequestHandler):
-  """List Club Details."""
-
-  def get(self):
-
-    url = "%s/api/list-clubs" % self.request.host_url
-
-    club_key = self.request.get('club-key', allow_multiple=True)
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    data = json.loads(result.content)
-
-    template_values = _get_standard_template_properties(self.request)
-    template_values['club_data'] = data
-
-    template = JINJA_ENVIRONMENT.get_template('list_clubs.html')
-    self.response.write(template.render(template_values))
+    with urllib.request.urlopen("http://csocgolf.appspot.com/" + url, timeout=120) as response:
+        html = response.read()
+        data = json.loads(html)
+        client.set(key, data, expire=3600)
+        return data
 
 
-class ListMembersGeneral(webapp2.RequestHandler):
-  """List Current Members Basic Info."""
 
-  def get(self):
+app = Flask(__name__)
 
-    daily_slope_rating_str = self.request.get(
-        'daily_slope_rating',
-        default_value=WOOLOOWARE_WHITE_SLOPE)
+@app.route('/')
+@app.route('/index')
+def index():
+    template_values = _get_standard_template_properties(request)
+    return render_template("index.html", **template_values)
 
-    daily_slope_rating = int(daily_slope_rating_str)
 
-    url = '%s/api/list-members' % self.request.host_url
-    result = urlfetch.fetch(url, follow_redirects=False,  deadline=60)
-    data = json.loads(result.content)
+@app.route('/list_members_general')
+def list_members_general():
 
-    sort_order = self.request.get('sort_by', 'member_no')
-    sort_direction = self.request.get('sort_direction', 'asc')
+    data = __retrieve_data("api/list-members")
+
+    # Determine sorting
+    sort_order = request.args.get('sort_by', 'member_no')
+    sort_direction = request.args.get('sort_direction', 'asc')
     reverse = sort_direction == 'desc'
     member_data = sorted(data, key=itemgetter(sort_order), reverse=reverse)
 
-    template_values = _get_standard_template_properties(self.request)
+    template_values = _get_standard_template_properties(request)
     template_values['member_list'] = member_data
-    template_values['path_url'] = self.request.path_url
-    template_values['show_pii'] = False
-    template_values['daily_slope_rating'] = daily_slope_rating / float(113)
-    template_values['daily_slope'] = daily_slope_rating_str
-
-    template = JINJA_ENVIRONMENT.get_template('list_members.html')
-    self.response.write(template.render(template_values))
+    template_values['daily_slope_rating'] = 118 / float(113)
+        
+    return render_template("list_members.html", **template_values)
 
 
-class ListMembers(webapp2.RequestHandler):
-  """List Current Members with PII information."""
-
-  def get(self):
-    url = '%s/api/list-members' % self.request.host_url
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    data = json.loads(result.content)
-
-    template_values = _get_standard_template_properties(self.request)
-    template_values['member_list'] = sorted(data, key=itemgetter('member_no'))
-    template_values['show_pii'] = True
-
-    template = JINJA_ENVIRONMENT.get_template('list_members.html')
-    self.response.write(template.render(template_values))
+@app.route('/show-latest-result')
+def show_latest_result():
+    pass
 
 
-class ShowMemberDetails(webapp2.RequestHandler):
-  """Show all scores + handicap calculations for a member."""
-
-  def get(self):
-    template_values = _get_standard_template_properties(self.request)
-
-    member_key = self.request.get('member_key')
-    adjust_score_for_win = self.request.get('adjust_score_for_win',
-                                            default_value="True")
+@app.route('/login-url')
+def login_url():
+    pass
 
 
-    if not member_key:
-      self.response.location(template_values['list_members_general'])
+@app.route('/list-club-general')
+def list_club_general():
 
-    url = '%s/api/get-scores?member_key=%s' % (self.request.host_url,
-                                               member_key)
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    data = json.loads(result.content)
+    template_values = _get_standard_template_properties(request)
+    template_values['club_data'] = __retrieve_data("api/list-clubs")
+    return render_template("list_clubs.html", **template_values)
 
-    url = '%s/api/get-member?member_key=%s' % (self.request.host_url,
-                                               member_key)
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    member_data = json.loads(result.content)
+
+@app.route('/show-match-result')
+def show_match_result():
+    """ Can show a detailed result, or a partial result. """
+
+
+    def _show_brief_match_result():
+            template_values = _get_standard_template_properties(request)
+            template_values['matches'] = __retrieve_data("api/get-match")
+            return render_template("brief_match_results.html", **template_values)
+
+
+    def __show_detailed_match_report(match_ker: str):
+            match_data = __retrieve_data("api/get-match?match_key=" + match_key)
+            scores = sorted(match_data['scores'], key=itemgetter('points'), reverse=True)
+
+            template_values = _get_standard_template_properties(request)
+            template_values['matches'] = match_data
+            template_values["scores"] = scores
+            template_values['prizes'] = match_data['prizes']
+            return render_template("detailed_match_results.html", **template_values)
+
+
+    match_key = request.args.get('match_key')
+
+    if match_key:
+        return __show_detailed_match_report(match_key)
+    else:
+        return _show_brief_match_result()      
+
+
+@app.route('/show-member-details')
+def show_member_details():
+    """ Prints all data about a specific member."""
+    member_key = request.args.get('member_key')
+
+    url = 'api/get-scores?member_key=' + member_key
+    data = __retrieve_data(url)
 
     score_data = []
     for score, hc_data in zip(data['scores'], data['handicap']['scores']):
@@ -168,213 +144,78 @@ class ShowMemberDetails(webapp2.RequestHandler):
       })
 
     score_data = sorted(score_data, key=itemgetter("date"), reverse=True)
+
+    template_values = _get_standard_template_properties(request)
     template_values['score_data'] = score_data
     template_values['hc_data'] = data['handicap']
     template_values['wins'] = data['wins']
-    template_values['member_data'] = member_data
-    template_values['handicap_multiplier'] = '0.93'
-    template = JINJA_ENVIRONMENT.get_template('show_member_details.html')
-    self.response.write(template.render(template_values))
+    template_values['member_data'] = data
+    template_values['handicap_multiplier'] = '0.93'    
+
+    return render_template("show_member_details.html", **template_values)
 
 
-class ShowLatestResult(webapp2.RequestHandler):
-
-  def get(self):
-    url = '%s/api/get-match' % self.request.host_url
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    match_data = json.loads(result.content)
-
-    location = '/show-match-result?match_key=%s' % match_data[0]['match_key']
-    self.redirect(location)
-
-
-class AddMatchResult(webapp2.RequestHandler):
-  """Adds a new result of a match."""
-
-  def get(self):
-    phase = self.request.get('phase')
-
-    if not phase:
-      self._phase_1()
-    else:
-      self._phase_2()
-
-
-  def _phase_1(self):
-    """User wants to select the Club the Game was played at."""
-
-    url = "%s/api/list-clubs" % self.request.host_url
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    data = json.loads(result.content)
-
-    clubs = [{'name': club['name'], 'key': club['key']} for club in data]
-    template_values = _get_standard_template_properties(self.request)
-    template_values['clubs'] = clubs
-
-    template = JINJA_ENVIRONMENT.get_template('add_match_result_phase_1.html')
-    self.response.write(template.render(template_values))
-
-  def _phase_2(self):
-    """We know the club, input the results."""
-    members_url = '%s/api/list-members-without-statistics' % self.request.host_url
-    result = urlfetch.fetch(members_url, follow_redirects=False, deadline=60)
-    member_data = json.loads(result.content)
-
-    club_key = self.request.get('club_key', None)
-    club_url = '%s/api/list-clubs?club_key=%s' % (self.request.host_url, club_key)
-    result = urlfetch.fetch(club_url, follow_redirects=False, deadline=60)
-    club_data = json.loads(result.content)
-
-    template_values = _get_standard_template_properties(self.request)
-    template_values['member_data'] = sorted(member_data, key=itemgetter('last_name'))
-    template_values['club_name'] = club_data[0]['name']
-    template_values['course_data'] = club_data[0]['courses']
-    template_values['max_player_results'] = 26
-    template_values['submit_score_url'] = '%s/api/add-match' % self.request.host_url
-    template_values['todays_date'] = date.today().strftime('%Y-%m-%d')
-    template = JINJA_ENVIRONMENT.get_template('add_match_result_phase_2.html')
-    self.response.write(template.render(template_values))
-
-
-class ShowMatchResult(webapp2.RequestHandler):
-
-  def get(self):
-    match_key = self.request.get('match_key')
-
-    if match_key:
-      self._show_detailed_match_results(match_key)
-    else:
-      self._show_brief_match_results()
-
-  def _show_detailed_match_results(self, match_key):
-    """Show full details about one match."""
-    url = '%s/api/get-match?match_key=%s' % (self.request.host_url, match_key)
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    match_data = json.loads(result.content)
-    scores = sorted(match_data['scores'], key=itemgetter('points'), reverse=True)
-
-    template_values = _get_standard_template_properties(self.request)
-    template_values['match'] = match_data
-    template_values['scores'] = scores
-    template_values['prizes'] = match_data['prizes']
-    template = JINJA_ENVIRONMENT.get_template('detailed_match_results.html')
-    self.response.write(template.render(template_values))
-
-  def _show_brief_match_results(self):
-    url = '%s/api/get-match' % self.request.host_url
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    match_data = json.loads(result.content)
-
-    template_values = _get_standard_template_properties(self.request)
-    template_values['matches'] = match_data
-    template_values['detailed_match_url'] = '%s/show-match-result' % self.request.host_url
-    template_values['edit_match_url'] = '%s/edit-match-result' % self.request.host_url
-    template = JINJA_ENVIRONMENT.get_template('brief_match_results.html')
-    self.response.write(template.render(template_values))
-
-
-class EditMatch(webapp2.RequestHandler):
-  """Edit a match restult, including delete."""
-
-  def get(self):
-    match_key = self.request.get('match_key')
-    url = '%s/api/get-match?match_key=%s' % (self.request.host_url, match_key)
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    match_data = json.loads(result.content)
-    scores = sorted(match_data['scores'], key=itemgetter('points'), reverse=True)
-
-    template_values = _get_standard_template_properties(self.request)
-    template_values['match'] = match_data
-    template_values['scores'] = scores
-    template_values['match_key'] = match_key
-    template_values['edit_score_url'] = '%s/edit-score-url' % self.request.host_url
-    template_values['delete_match_url'] = '%s/delete-match' % self.request.host_url
-    template = JINJA_ENVIRONMENT.get_template('edit_match_result.html')
-    self.response.write(template.render(template_values))
-
-  def post(self):
+@app.route('/add-members-url')
+def add_members_url():
     pass
 
+@app.route('/add-club-url')
+def add_club_url():
+    pass
 
-class DeleteMatch(webapp2.RequestHandler):
+@app.route('/add-match-result-url')
+def add_match_result_url():
+    
+    def __phase_1():
+        data = __retrieve_data("api/list-clubs")
+        clubs = [{'name': club['name'], 'key': club['key']} for club in data]
+        template_values = _get_standard_template_properties(request)
+        template_values['clubs'] = clubs
 
-  def get(self):
-    match_key = self.request.get('match_key')
-    url = '%s/api/delete-match?match_key=%s' % (self.request.host_url, match_key)
-    result = urlfetch.fetch(url, follow_redirects=False, deadline=60)
-    delete_result = json.loads(result.content)
+        return render_template("add_match_result_phase_1.html", **template_values)
 
-    template_values = _get_standard_template_properties(self.request)
-    template_values['delete_result'] = delete_result
-    template = JINJA_ENVIRONMENT.get_template('delete_match_result.html')
-    self.response.write(template.render(template_values))
+    def __phase_2():
+        
+        member_data = __retrieve_data("api/list-members-without-statistics")
 
+        club_key = request.args.get('club_key', None)
+        club_data = __retrieve_data("api/list-clubs?club_key=" + club_key)
 
+        template_values = _get_standard_template_properties(request)
+        template_values['member_data'] = sorted(member_data, key=itemgetter('last_name'))
+        template_values['club_name'] = club_data[0]['name']
+        template_values['course_data'] = club_data[0]['courses']
+        template_values['max_player_results'] = 26
+        template_values['submit_score_url'] = 'https://csocgolf.appspot.com/api/add-match'
+        template_values['todays_date'] = date.today().strftime('%Y-%m-%d')
 
-class ShowFailure(webapp2.RequestHandler):
-  """Show a generic failure message."""
+        return render_template("add_match_result_phase_2.html", **template_values)
 
-  def get(self):
-    template_values = _get_standard_template_properties(self.request)
-    template_values['heading'] = self.request.get('heading')
-    template_values['message'] = self.request.get('message')
+    phase = request.args.get('phase')
 
-    template = JINJA_ENVIRONMENT.get_template('failure.html')
-    self.response.write(template.render(template_values))
+    if not phase:
+        return __phase_1()
+    else:
+        return __phase_2()
 
+@app.route('/list-member-admin-url')
+def list_member_admin_url():
+    pass
 
-def _get_standard_template_properties(request):
-  """Return a dictionary of the standard template values."""
-  if users.get_current_user():
-    url = users.create_logout_url(request.uri)
-    url_linktext = 'Logout'
-  else:
-    url = users.create_login_url(request.uri)
-    url_linktext = 'Login'
+def _get_standard_template_properties(request_data):
+    template_values = {
+        'admin_user': True,
+    }
 
-  parse_result = urlparse(request.path_url)
-
-  base =  '%s://%s' % (parse_result.scheme, parse_result.netloc)
-
-  template_values = {
-    # Top Navbar Links
-    'home_url': base,
-    'login_url': url,
-    'login_url_linktext': url_linktext,
-    # General Links
-    'list_members_general': '%s/list-members-general' % base,
-    'list_club_general': '%s/list-club-general' % base,
-    'show_match_result': '%s/show-match-result' % base,
-    'show_member_details': '%s/show-member-details' % base,
-    'show_latest_result': '%s/show-latest-result' % base,
-    # Admin Links
-    'add_club_url': '%s/add-club' % base,
-    'add_members_url': '%s/add-members' % base,
-    'list_member_admin_url': '%s/list-members-admin' % base,
-    'add_match_result_url': '%s/add-match-result' % base,
-    'admin_user': users.is_current_user_admin(),
-  }
-
-  return template_values
+    return template_values
 
 
-app = webapp2.WSGIApplication([
-  ('/', MainPage),
-  # Club Functions
-  ('/list-members-general', ListMembersGeneral),
-  ('/list-club-general', ListClubGeneral),
-  ('/show-match-result', ShowMatchResult),
-  ('/show-member-details', ShowMemberDetails),
-  ('/show-latest-result', ShowLatestResult),
-
-  # Admin Functions
-  ('/add-club', AddClub),
-  ('/add-members', AddMembers),
-  ('/add-match-result', AddMatchResult),
-  ('/list-members-admin', ListMembers),
-  ('/edit-match-result', EditMatch),
-  ('/delete-match', DeleteMatch),
-
-  # Generic Help/Error Pages
-  ('/failure', ShowFailure),
-], debug=True)
+if __name__ == "__main__":
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host="127.0.0.1", port=8080, debug=True)
